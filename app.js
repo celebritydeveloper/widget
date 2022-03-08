5480e91f966a96a9e580a3b0722bc992c7ab1872
Saviour Essien <31137610+celebritydeveloper@users.noreply.github.com>
2022-03-08 17:19:36 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+import { Feedjet } from "https://res.cloudinary.com/spire-tech/raw/upload/v1646754471/widget-cdn/Feedjet_oi8s81.js";
+// import { feedblocks } from './query.js'
+
+//the feedblocks data from the graphQL server
+// const feedblocksData = feedblocks.data.listConsumerFeedblocks.data
+
+const feedjet = new Feedjet({position: "bottom-right"});
+
+
+
